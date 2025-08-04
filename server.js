4129695eff62301b0f0b3d177fd751701768f303
Pullaiahgari Manideep Reddy <161033514+manideep9243@users.noreply.github.com>
2025-08-04 12:12:38 +0530

    externalschema

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const axios = require('axios');
 const app = express();
 const port = 3000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'Uploads')));
